7b486bae6dff3bc7ae31e1d83779eb1d43c5c4e7
Lijin Thekkekara <lijinj@gmail.com>
2021-11-25 23:31:44 +0100

    Create superhero avatar

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
 gem 'jquery-rails'
+gem 'faker', '~> 2.19'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
+    before_action :common_content, :only => [:index, :team]
+
+    def common_content
+        @name = Faker::Superhero.name
+        @power = Faker::Superhero.power
+        @corp_title = (Faker::Job.title).titleize
+        @corp_power = (Faker::Company.bs).capitalize
+
+        @rand_num = rand(1..100)
+        @set = "set" + rand(1..2).to_s
+        @bg = "bg" + rand(1..2).to_s
+
+        @avatar = Faker::Avatar.image(slug: @rand_num, size: "300x300", format: "png", set: @set, bgset: @bg)
+    end
+
     def index
     end
 
